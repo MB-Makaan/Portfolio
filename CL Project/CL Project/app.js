2d471c7ec56b6b12cdbef6088c93bc04c21bfb7b
Matthew Brown <43123226+MB-Makaan@users.noreply.github.com>
2018-11-15 18:49:55 -0500

    Add files via upload
    Final Commit for Project

---
+++ CL Project/CL Project/app.js
@@ -0,0 +1,26 @@
+function showHide() {
+    var x = document.getElementById("hidden");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function hideShow() {
+    var x = document.getElementById("hidE");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function goAway() {
+    var x = document.getElementById("hIde");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
---
+++ CL Project/Readme for Code Louisville Project.txt
@@ -0,0 +1,3 @@
+This project is a Portfolio and will be the entry page to show other projects I am working on. It also will serve as
+a primer for information about myself.  The site is set with a media first approach, and to access the Javascript on the site
+you will need to use the navigation menu and click on "Things I enjoy" to move to the correct page.
